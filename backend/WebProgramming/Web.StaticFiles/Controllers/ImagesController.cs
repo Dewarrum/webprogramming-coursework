a9bd60c254b569ebbcd454eb604bb259048690ad
Yuri Tsai <yura.tsai@gmail.com>
2020-02-28 23:39:32 +0700

    Initiated Web.StaticFiles controller with Images Controller.

---
+++ backend/WebProgramming/Web.StaticFiles/Controllers/ImagesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.StaticFiles.Models.Images;
+
+namespace Web.StaticFiles.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImagesController : ControllerBase
+    {
+        private const string Domain = "http://localhost:7000";
+        
+        [HttpPost("Upload")]
+        public async Task<ActionResult> Upload([FromForm(Name = "images")] IFormFileCollection files)
+        {
+            var model = new UploadModel();
+
+            foreach (var file in files)
+            {
+                var fileName = Guid.NewGuid().ToString("N") + Path.GetExtension(file.FileName);
+                var directory = Path.Combine("wwwroot", "Images", "Raws", fileName);
+
+                await using var stream = System.IO.File.Create(directory);
+                await file.CopyToAsync(stream);
+                
+                model.Images.Add(new UploadModel.ImageInfo{Url = $"{Domain}/Images/Raws/{fileName}"});
+            }
+            
+            model.Date = DateTime.UtcNow;
+
+            return Ok(model);
+        }
+    }
+}
---
+++ backend/WebProgramming/Web.StaticFiles/Models/Images/UploadModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web.StaticFiles.Models.Images
+{
+    public class UploadModel
+    {
+        public int Count => Images.Count;
+        public DateTime Date { get; set; }
+
+        public IList<ImageInfo> Images { get; private set; }
+
+        public UploadModel()
+        {
+            Images = new List<ImageInfo>();
+        }
+
+        public class ImageInfo
+        {
+            public string Url { get; set; }
+        }
+    }
+}
