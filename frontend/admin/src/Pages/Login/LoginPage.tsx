b780bf73bad1834b1d189ac00de75afd9eb03432
Yuri Tsai <yura.tsai@gmail.com>
2020-02-28 23:43:20 +0700

    Updated login form.

--- frontend/admin/src/Pages/Login/LoginPage.tsx
+++ frontend/admin/src/Pages/Login/LoginPage.tsx
@@ -2,35 +2,70 @@ import React, {FormEvent, useState} from "react";
 import AuthManager from "../Authorization/AuthManager";
 import { Redirect, useHistory } from "react-router-dom";
 import {buildUrl, routes} from "../../Routes/Routes";
-import {Button, Card, CardBody, Container, Input, Label} from "reactstrap";
+import {Button, Card, CardBody, CardHeader, Container, Form, FormGroup, Input, Label, Spinner} from "reactstrap";
+
+interface IState {
+    loading: boolean;
+    formData: any;
+}
 
 const LoginPage: React.FC = () => {
-    const [state, setState] = useState({});
+    const [state, setState] = useState({
+        loading: false,
+        formData: {}
+    } as IState);
     const history = useHistory();
 
     const handleChange = (e : FormEvent<HTMLInputElement>) => {
-        const newState: any = state;
-        newState[e.currentTarget.name] = e.currentTarget.value;
+        const newState: IState = state;
+        newState.formData[e.currentTarget.name] = e.currentTarget.value;
 
         setState(newState);
     };
 
     const onAuth = (token: string | null, succeeded: boolean) => {
-        history.push("/");
+        setState({
+            loading: succeeded,
+            formData: state.formData
+        });
+
+        if (succeeded) {
+            history.push("/");
+        }
+    };
+
+    const onLoginClicked = () => {
+        setState({
+            loading: true,
+            formData: state.formData
+        });
+
+        AuthManager.login(state.formData, onAuth);
     };
 
     return AuthManager.userSession ? (
         <Redirect to={buildUrl(routes.home)} />
     ) : (
-        <Container>
+        <Container className="col-6 offset-3 mt-5">
             <Card>
+                <CardHeader>
+                    <h4>Log in WebProgramming Coursework</h4>
+                </CardHeader>
                 <CardBody>
-                    <h1>Login</h1>
-                    <Label for="login">Login</Label>
-                    <Input id="login" name="login" onChange={handleChange}/>
-                    <Label for="password">Password</Label>
-                    <Input id="password" name="password" onChange={handleChange}/>
-                    <Button color="primary" onClick={() => AuthManager.login(state, onAuth)}>Login</Button>
+                    <Form>
+                        <FormGroup>
+                            <Label for="login">Login</Label>
+                            <Input type="text" id="login" name="login" onChange={handleChange}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="password">Password</Label>
+                            <Input type="password" id="password" name="password" onChange={handleChange}/>
+                        </FormGroup>
+                        <Button color="primary" disabled={state.loading} onClick={onLoginClicked}>
+                            {state.loading ? (<Spinner size="sm" className="mr-1" />) : null}
+                            Login
+                        </Button>
+                    </Form>
                 </CardBody>
             </Card>
         </Container>
